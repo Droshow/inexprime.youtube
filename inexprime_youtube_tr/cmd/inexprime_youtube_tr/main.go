18151b6ea79a1c74744929913db0b09ba457ee51
Dro-Convex <martin.drotar@convexin.com>
2024-04-08 16:49:24 +0200

    added video parser

--- inexprime_youtube_tr/cmd/inexprime_youtube_tr/main.go
+++ inexprime_youtube_tr/cmd/inexprime_youtube_tr/main.go
@@ -2,21 +2,59 @@
 package main
 
 import (
-    "fmt"
-    "youtube_tracker/pkg/youtube"
+	"encoding/json"
+	"fmt"
+	"youtube_tracker/pkg/youtube"
 )
 
+// Defining parser struct
+type Video struct {
+	Items []struct {
+		Snippet struct {
+			Title       string `json:"title"`
+			Description string `json:"description"`
+		} `json:"snippet"`
+	} `json:"items"`
+}
+
+// Parsing video details
+func parseVideoDetails(videoDetails string) (string, string, error) {
+	var video Video
+	err := json.Unmarshal([]byte(videoDetails), &video)
+	if err != nil {
+		return "", "", err
+	}
+
+	title := video.Items[0].Snippet.Title
+	description := video.Items[0].Snippet.Description
+
+	return title, description, nil
+}
+
 func main() {
-    //TODO: has to go somewhere into by UI useable file
-    videoID := "0hBQBhinxeU"
-    //TODO: has to be hid somewhere in secrets
-    apiKey := "AIzaSyCtebTX6Qn1m1DL6ZpsRBv-czqBW7PkpCk"
+	//TODO: has to go somewhere into by UI useable file
+	videoID := "0hBQBhinxeU"
+	//TODO: has to be hid somewhere in secrets
+	apiKey := "AIzaSyCtebTX6Qn1m1DL6ZpsRBv-czqBW7PkpCk"
 
-    videoDetails, err := youtube.FetchVideoDetails(videoID, apiKey)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	videoDetails, err := youtube.FetchVideoDetails(videoID, apiKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    fmt.Println(videoDetails)
+	title, description, err := parseVideoDetails(videoDetails)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Limit the description to the first 100 characters
+	if len(description) > 200 {
+		description = description[:200] + "..."
+	}
+
+	fmt.Println("Title:", title)
+	fmt.Println("Description:", description)
+
 }
